740336dfce7294ee2cdfa851b925066c408b538a
pimaja <maja.piskac@gmail.com>
2019-05-21 10:16:18 +0100

    forme za filer i sort

--- Projekt/view/sortiraj_filtriraj.php
+++ Projekt/view/sortiraj_filtriraj.php
@@ -9,7 +9,13 @@ Odaberite kriterije po kojima želite sortirati:
 <input type="textbox" name="sort" value = "broj_zvjezdica"> Broj zvjezdica <br />
 <br />
 Odaberite kriterij/e po kojima želite filtrirati:
-
+<br />
+<input type="textbox" name="filter" value = "cijena"> Cijena iznosa <input type="text"> <br />
+<input type="textbox" name="filter" value = "udaljenost"> Udaljenost od centra iznosa <input type="text"> km <br />
+<input type="textbox" name="filter" value = "broj_osoba"> Broj osoba u sobi <input type="text"> <br />
+<input type="textbox" name="filter" value = "ocjena"> Ocjena (1-10) <input type="text"> <br />
+<input type="textbox" name="filter" value = "broj_zvjezdica"> Broj zvjezdica (1-5) <input type="text"> <br />
+<br />
 <button type="submit" name="button">Dalje</button><br /><br />
 <button type="submit" name="odlogiraj">Odlogiraj se</button>
 </form>
